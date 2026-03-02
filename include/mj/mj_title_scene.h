2aab56e1831e18ae85cfeb0432c035cfdcd1d284
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-03-02 00:45:15 +0000

    Deleted unneeded fields from mj_title_scene

--- include/mj/mj_title_scene.h
+++ include/mj/mj_title_scene.h
@@ -35,8 +35,6 @@ private:
     bn::optional<bn::bg_palettes_fade_to_action> _bgs_fade_action;
     bn::optional<bn::sprite_palettes_fade_to_action> _sprites_fade_action;
     bn::optional<bn::music_volume_to_action> _music_volume_action;
-    bn::vector<bn::sprite_ptr, 16> _title_sprites_1;
-    bn::vector<bn::sprite_ptr, 16> _title_sprites_2;
     bn::vector<bn::sprite_ptr, 8> _high_score_sprites;
     bn::vector<bn::sprite_ptr, 4> _play_sprites;
     bn::vector<bn::sprite_ptr, 4> _credits_sprites;
@@ -47,10 +45,6 @@ private:
     bn::sprite_affine_mat_attributes_hbe_ptr _affine_mat_hbe;
 
     void _update_bgs();
-
-    //void _create_title_sprites();
-
-    //void _update_title_sprites(bool update_first);
 
     void _set_menu_visible(bool visible);
 
