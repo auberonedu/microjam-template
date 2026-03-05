2908f884ae377445745151021e11f1a41e65777e
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-03-05 01:56:45 +0000

    Game list in chooser

--- include/mj/mj_chooser_scene.h
+++ include/mj/mj_chooser_scene.h
@@ -1,6 +1,7 @@
 #ifndef MJ_CHOOSER_SCENE_H
 #define MJ_CHOOSER_SCENE_H
 
+#include <bn_string.h>
 #include <bn_vector.h>
 #include <bn_sprite_ptr.h>
 
@@ -22,7 +23,9 @@ public:
 private:
     core& _core;
     int _cursor_idx;
-    bn::vector<bn::sprite_ptr, 16*64> _game_names;
+    bn::vector<bn::sprite_ptr, 16*64> _game_name_sprites;
+
+    bn::vector<bn::string<16>, 64> _game_names;
 };
 
 }
--- src/mj/mj_chooser_scene.cpp
+++ src/mj/mj_chooser_scene.cpp
@@ -1,15 +1,41 @@
 #include "mj_chooser_scene.h"
 #include "mj_scene_type.h"
 
+#include "mj_core.h"
+#include "mj_game_list.h"
+#include "mj_game.h"
+
+#include <bn_unique_ptr.h>
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_keypad.h>
+#include <bn_fixed_point.h>
 
 namespace mj {
 
 chooser_scene::chooser_scene(core& core) :
     _core(core) {
         bn::backdrop::set_color(bn::color(31, 0, 0));
+        auto& text_generator = _core.small_text_generator();
+        game_data fake_game_data = {
+            core.text_generator(),
+            core.small_text_generator(),
+            core.big_text_generator(),
+            core.random(),
+            300
+        };
+        int y = -60;
+        bn::unique_ptr<mj::game> game;
+        bn::span<game_list::function_type> game_list_entries = game_list::get();
+        for(int i = 0; i < game_list_entries.size(); i++) {
+            game_list::function_type game_list_entry = game_list_entries[i];
+            game.reset(game_list_entry(0, fake_game_data));
+            auto title = game->title();
+            _game_names.push_back(title);
+            text_generator.generate({0, y}, title, _game_name_sprites);
+            y += 10;
+        }
+        game.reset();
     }
 
 [[nodiscard]] bn::optional<scene_type> chooser_scene::update() {
