6aa72a340fb519a23bb605d6aad5615a9b61b394
SamuelM007 <174657033+SamuelM007@users.noreply.github.com>
2026-03-07 20:32:47 +0000

    finsihed step 4 of dummy implementation

---
+++ src/mar/mar_mars_escape.cpp
@@ -0,0 +1,9 @@
+#include "mar_mars_escape.h"
+
+namespace mar{
+
+mar_mars_escape::mar_mars_escape([[maybe_unused]] int completed_games, [[maybe_unused]] const mj::game_data& data) :
+    mj::game("mar")
+    {}
+
+}
