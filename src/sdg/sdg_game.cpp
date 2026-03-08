046160883c35c5e9eeed50b039fb6dd43f186653
DennysSaenz <SaenzSegovia.Dennys@student.greenriver.edu>
2026-03-08 06:05:04 +0000

    Completed last step on wave 4

--- src/sdg/sdg_game.cpp
+++ src/sdg/sdg_game.cpp
@@ -1,4 +1,20 @@
 #include "sdg_game.h"
+#include "mj/mj_game_list.h"
+
+namespace
+{
+    constexpr bn::string_view code_credits[] = { "Iker & Kevin" };
+    constexpr bn::string_view graphics_credits[] = { "Kevin" };
+    constexpr bn::string_view sfx_credits[] = {""};
+    constexpr bn::string_view music_credits[] = {""};
+}
+
+// Macros used to add game to game list
+MJ_GAME_LIST_ADD(sdg::sdg_game) // The class of the game is used here
+MJ_GAME_LIST_ADD_CODE_CREDITS(code_credits)
+MJ_GAME_LIST_ADD_GRAPHICS_CREDITS(graphics_credits)
+MJ_GAME_LIST_ADD_MUSIC_CREDITS(music_credits)
+MJ_GAME_LIST_ADD_SFX_CREDITS(sfx_credits)
 
 namespace sdg{
     sdg_game::sdg_game([[maybe_unused]] int completed_games, [[maybe_unused]] const mj::game_data& data) :
