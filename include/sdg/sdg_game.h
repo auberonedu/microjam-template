8633dabf48dd93630bb417fe8e9e1048e9e34195
DennysSaenz <SaenzSegovia.Dennys@student.greenriver.edu>
2026-03-08 05:14:08 +0000

    Completed wave 4 part 1

---
+++ include/sdg/sdg_game.h
@@ -0,0 +1,29 @@
+// Include guard 
+#ifndef SDG_GAME_H 
+#define SGD_GAME_H
+
+#include "mj/mj_game.h"
+namespace sdg{
+    class sdg_game : public mj::game {
+    public:
+    // Constructor
+    sdg_game(int completed_games, const mj::game_data& data);
+
+    // Required overrides
+    bn::string<16> title() const override;
+
+    int total_frames() const override;
+
+    mj::game_result play(const mj::game_data& data) override;
+
+    bool victory() const override;
+
+    
+    void fade_in(const mj::game_data& data) override;
+
+    
+    void fade_out(const mj::game_data& data) override;
+}; 
+}
+
+#endif
